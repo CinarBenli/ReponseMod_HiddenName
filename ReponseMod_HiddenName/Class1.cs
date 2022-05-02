9a062c6ea3c911f944acfdfbef7043e3aed31b18
lenovo <lenovo@DESKTOP-UGBTSUO>
2022-05-02 18:08:40 +0300

    Proje dosyası ekle.

---
+++ ReponseMod_HiddenName/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReponseMod_HiddenName
+{
+    public class Class1 : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            base.Load();
+            U.Events.OnPlayerConnected += j;
+        }
+
+        private void j(UnturnedPlayer player)
+        {
+            if (player.HasPermission(Configuration.Instance.PolicePerm) || player.HasPermission(Configuration.Instance.DoctorPerm) || player.HasPermission(Configuration.Instance.AdminPerm))
+            {
+                player.Player.disablePluginWidgetFlag(SDG.Unturned.EPluginWidgetFlags.ShowInteractWithEnemy);
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ ReponseMod_HiddenName/Config.cs
@@ -0,0 +1,16 @@
+﻿using Rocket.API;
+
+namespace ReponseMod_HiddenName
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string PolicePerm;
+        public string DoctorPerm;
+        public string AdminPerm;
+
+        public void LoadDefaults()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
